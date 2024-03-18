8e3e318a0b4abbf27dd3b3645c3f6b2a1947d2a0
Volotka <s.volotka73@gmail.com>
2024-03-18 16:06:31 +0600

    Создан проект

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var historyOfChangesTextView: UITextView!
+    @IBOutlet weak var counterUILabel: UILabel!
+    private var counter: Int = 0
+    private var formatter = DateFormatter()
+    private var dateText = ""
+        
+    
+    @IBOutlet weak var addUIButton: UIButton!
+    
+    @IBOutlet weak var subtractUIButton: UIButton!
+    @IBOutlet weak var resetUIButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        historyOfChangesTextView.text = "История изменений:\n"
+        formatter.dateFormat = "dd/MM/yyyy' 'HH:mm:ss"
+        
+        // counterUILabel.text = counterText
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addNumber(_ sender: Any) {
+        var currentData = Date()
+        dateText = formatter.string(from: currentData)
+        counter += 1
+        counterUILabel.text = "Значение счётчика: \(counter)"
+        historyOfChangesTextView.text += "\(dateText): значение изменено на +1\n"
+    }
+    
+    @IBAction func subtractNumber(_ sender: Any) {
+         var currentData = Date()
+        dateText = formatter.string(from: currentData)
+        counter -= 1
+        if counter < 0 {
+            counter = 0
+            historyOfChangesTextView.text += "\(dateText): попытка уменьшить значение счётчика ниже 0\n"
+        } else {
+            historyOfChangesTextView.text += "\(dateText): значение изменено на -1\n"
+        }
+        counterUILabel.text = "Значение счётчика: \(counter)"
+        }
+    
+    @IBAction func resetCounter(_ sender: Any) {
+         var currentData = Date()
+        dateText = formatter.string(from: currentData)
+        counter = 0
+        counterUILabel.text = "Значение счётчика: \(counter)"
+        historyOfChangesTextView.text += "\(dateText): значение сброшено\n"
+    }
 }
 
